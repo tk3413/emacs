5919bf222669f1ac0eb97b9ae404aee86770461a
Taimore Khan <khan.taimore@gmail.com>
2023-02-05 00:49:52 -0500

    removed extra stuff, formatted

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(exec-path-from-shell cider solarized-theme add-node-modules-path js-auto-format-mode xref-js2 js2-refactor js2-mode treemacs terraform-mode which-key rust-mode feature-mode elpy magit yaml-mode json-mode rainbow-delimiters highlight-indent-guides define-word company rainbow-mode zenburn-theme use-package)))
+   '(auto-dim-other-buffers company-box terraform-doc lsp-java-boot java-imports interaction-log helm-lsp lsp-java lsp-ui lsp-mode flycheck projectile solarized solarized-light exec-path-from-shell cider solarized-theme add-node-modules-path js-auto-format-mode xref-js2 js2-refactor js2-mode treemacs terraform-mode which-key rust-mode feature-mode elpy magit yaml-mode json-mode rainbow-delimiters highlight-indent-guides define-word company rainbow-mode zenburn-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
